ef531627529ddf27a23f75dfbe0219c4e1c786c1
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2025-05-10 14:27:21 +0100

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef } from "react";
+
 const SearchBar: React.FC = (): React.JSX.Element => {
+	const inputRef = useRef(null as HTMLInputElement | null);
+
 	// Search the query on Google Search
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -7,12 +11,20 @@ const SearchBar: React.FC = (): React.JSX.Element => {
 		window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
 	};
 
+	useEffect(() => {
+		const input = inputRef.current;
+		if (input) {
+			input.focus();
+		}
+	}, []);
+
 	return (
 		<form
 			className="flex items-center justify-center w-full h-16 bg-zinc-800 mt-12 px-4"
 			onSubmit={handleSubmit}
 		>
 			<input
+				ref={inputRef}
 				type="text"
 				placeholder="Search..."
 				className="sm:w-[600px] w-full p-2 text-lg text-white border border-pink-200 rounded-md focus:outline-none focus:ring focus:ring-pink-200"
